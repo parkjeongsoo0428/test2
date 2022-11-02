74a54825967ed2e4f7eed38436178f560ceaeb57
qwertas05 <qwertas05@naver.com>
2022-11-02 10:46:29 +0900

    test

---
+++ StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String 문자열
+		// System.out.println('Hello World'); // Character 문자
+		System.out.println("A"); // String
+		
+		System.out.println("Hello"
+				+ " World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		// 에러 System.out.println("hello "world"");
+		// escape
+		System.out.println("Hello \"Wolrd\""); //Hello "World"
+	}
+
+}
